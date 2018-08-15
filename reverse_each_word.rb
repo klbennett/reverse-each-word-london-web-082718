eb46b7a450ebefc5c6888098a1a4f3d3562f104d
Kay B <kaylouisebennett@gmail.com>
2018-08-15 12:03:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   new_sentence = sentence.split
   reverse_sentence = []
   new_sentence.each do |x|
-    reverse_sentence << x.reverse
+   puts reverse_sentence << x.reverse
   end
 end
